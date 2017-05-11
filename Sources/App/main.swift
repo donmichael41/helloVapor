857af393b63439c244670daa86cb369dbaf73e44
Hung Nguyen <dev.hungnguyen@gmail.com>
2017-05-11 19:20:05 +0700

    Change alit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,25 @@
 import Vapor
 import HTTP
-let drop = Droplet()
+import VaporPostgreSQL
 
+let drop = Droplet(providers:
+    [VaporPostgreSQL.Provider.self]
+)
 
 let weatherController = WeatherController()
 weatherController.addRoutes(drop: drop)
 
 drop.client = FoundationClient.self
+
+drop.get("version") { _ in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        
+        return try JSON(node: version)
+    }
+    
+    return "No db"
+}
 
 drop.get("loaderio-822a5198e007a78606027c6f7d2f3624") { _ in
         return "loaderio-822a5198e007a78606027c6f7d2f3624"
